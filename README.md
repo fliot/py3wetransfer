d3a059e65ad4cbba4f7fa491f34a6c7469e90a1d
fliot <francois@liot.org>
2018-09-29 13:04:52 +0200

    Board implementation

--- README.md
+++ README.md
@@ -12,14 +12,17 @@ pip install --index-url https://test.pypi.org/simple/ py3wetransfer
 ```
 
 # Functional features
-  - single upload
-  - multiple uploads
-  - board (very soon)
+  - Transfer API (single upload and multiple uploads)
+https://wetransfer.github.io/wt-api-docs/index.html#transfer-api
+  - Board API
+https://wetransfer.github.io/wt-api-docs/index.html#board-api
 
 # Usage
-**Before starting make sure you have an API key acquired from [Developers Portal](https://developers.wetransfer.com/).**
+**Before starting, make sure you have an API key acquired from [Developers Portal](https://developers.wetransfer.com/).**
 
-To initialize the client, you need to use your own api key, and your own user identifier. 
+To initialize the client, you need to use your own api key. 
+
+# Transfer
 
 **upload_file**
 
@@ -45,7 +48,32 @@ print( x.upload_files( ["file1.zip", "file2.zip"] , "test upload") )
 >> "https://we.tl/t-ajQpdqGxco"
 ```
 
-Do it with debug
+# Board
+
+**Manage board**
+
+```python
+from py3wetransfer import Py3WeTransfer
+
+x = Py3WeTransfer("<my-very-personal-api-key>")
+
+[ board_id, board_url ] = x.create_new_board("test board")
+
+print(board_url)
+>> "https://we.tl/t-ajQpdqGxco"
+
+# add links
+x.add_links_to_board( board_id, [{"url": "https://wetransfer.com/", "title": "WeTransfer"}] )
+
+# add files
+x.add_files_to_board( board_id, ["test1.png", "test2.jpg"] )
+
+# retrieve the board object 
+# https://wetransfer.github.io/wt-api-docs/index.html#retrieve-boards-information
+board_object = x.get_board( board_id )
+```
+
+# Debug
 ```python
 from py3wetransfer import Py3WeTransfer
 
@@ -55,10 +83,18 @@ print( x.upload_file("test.zip", "test upload") )
 ...
 ```
 
-# Complete optionnal parameters
-WeTransfer asks officially for a valid "domain_user_id"/"user_identifier" in their API documentation, but in practise, it perfectly works without providing it, but you can also provide it if you really want...
+# Testing authentication
+If you need to test authentication validity
+```python
+from py3wetransfer import Py3WeTransfer
 
-You can use a complete debug logging, including http request, requests library and py3wetransfer logs.
+x = Py3WeTransfer("xA8ZYoVox57QfxX77hjQ2AI7hqO6l9M4tqv8b57c", debug=True)
+
+if x.isAuthentified() : print("we are authentified)
+```
+
+# Additionnal authentication parameters
+WeTransfer asks officially for a valid "domain_user_id"/"user_identifier" in their API documentation, but in practise, it perfectly works without providing it, but you can also provide it if you really want...
 ```python
 from py3wetransfer import Py3WeTransfer
 
--- py3wetransfer/__init__.py
+++ py3wetransfer/__init__.py
@@ -42,7 +42,90 @@ class Py3WeTransfer(object):
         except :
             print(r.text)
     
+    def isAuthentified(self):
+        if self.token != "" : return True
+        else : return False
     
+    # presigned amazon s3 handler (common to board and transfer API)
+    def file_upload(self, url, file_name, mime_type, bytes_stream):
+        headers = {'Content-Type': mime_type, 'File': file_name}
+        r = requests.put(url, data=bytes_stream)
+    
+    ##################################################################################################################################
+    # Board API https://wetransfer.github.io/wt-api-docs/index.html#board-api
+    ##################################################################################################################################
+    def get_board(self, board_id):
+        address = 'https://dev.wetransfer.com/v2/boards/%s' % board_id
+        headers = { "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
+        r = requests.get(address, headers=headers)
+        if self.debug : print(r.text)
+        return (json.loads(r.text))
+    
+    def create_new_board(self, name):
+        address = 'https://dev.wetransfer.com/v2/boards'
+        headers = { "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
+        data    = { "name":name }
+        if self.debug : print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+        r = requests.post(address, headers=headers, data=json.dumps(data))
+        if self.debug : print(r.text)
+        return [ (json.loads(r.text))['id'], (json.loads(r.text))['url'] ]
+    
+    def add_links_to_board(self, board_id, data):
+        address = 'https://dev.wetransfer.com/v2/boards/%s/links' % board_id
+        headers={ "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
+        r = requests.post(address, headers=headers, data=json.dumps(data))
+        if self.debug : print(r.text)
+        return self.get_board(board_id)
+    
+    def add_files_to_board(self, board_id, file_paths):
+        files = []
+        for file_path in file_paths :
+            files.append( { 'file_path': file_path,
+                            'file_name': os.path.basename(file_path),
+                            'file_size': os.path.getsize(file_path),
+                            'mime_type': mime.from_file(file_path) } )
+        
+        address = 'https://dev.wetransfer.com/v2/boards/%s/files' % board_id
+        headers={ "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
+        data = []
+        for i in files: data.append( { "name": i['file_name'], 'size': i['file_size'] } )
+        if self.debug : print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+        r = requests.post(address, headers=headers, data=json.dumps(data))
+        if self.debug : print(r.text)
+        upload_plans = json.loads(r.text)
+        
+        for num in range(0, len(upload_plans)):
+            f = files[num]
+            up = upload_plans[num]
+            i = 1
+            fr = open(f['file_path'], "rb")
+            bytes_read = fr.read(up['multipart']['chunk_size'])
+            while bytes_read:
+                url = self.request_upload_url_board(board_id, up['id'], i, up['multipart']['id'])
+                self.file_upload(url, f['file_name'], f['mime_type'], bytes_read)
+                bytes_read = fr.read(up['multipart']['chunk_size'])
+                i += 1
+            fr.close()
+            self.complete_file_upload_board(board_id, up['id'])
+        return self.get_board(board_id)
+    
+    def request_upload_url_board(self, board_id, file_id, part_number, multipart_upload_id):
+        address = 'https://dev.wetransfer.com/v2/boards/%s/files/%s/upload-url/%s/%s' % (board_id, file_id, part_number, multipart_upload_id)
+        headers={ "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
+        r = requests.get(address, headers=headers)
+        if self.debug : print(r.text)
+        return (json.loads(r.text))['url']
+    
+    def complete_file_upload_board(self, board_id, file_id):
+        address = 'https://dev.wetransfer.com/v2/boards/%s/files/%s/upload-complete' % (board_id, file_id)
+        headers = { "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
+        r = requests.put(address, headers=headers)
+        if self.debug : print(r.text)    
+    
+    
+    ##################################################################################################################################
+    # Transfer API https://wetransfer.github.io/wt-api-docs/index.html#transfer-api
+    ##################################################################################################################################
     def upload_file(self, file_path, message):
         return self.upload_files([file_path], message)
     
@@ -71,7 +154,6 @@ class Py3WeTransfer(object):
                 self.complete_file_upload(transfer_id, f['file_id'], f['part_numbers'])
             return self.finalize_transfer(transfer_id)
     
-    
         else : 
             # take care, this uses wetransfer API V4, which is currently undocumented
             [transfer_id, files] = self.create_new_transfer_mail(message, files, self.sender, self.recipients, self.language)
@@ -96,7 +178,6 @@ class Py3WeTransfer(object):
                 self.complete_file_upload_mail(transfer_id, f['file_id'], f['part_numbers'])
                 part_numbers += f['part_numbers']
             return self.finalize_transfer_mail(transfer_id, part_numbers)
-    
     
     def create_new_transfer(self, message, files):
         address = 'https://dev.wetransfer.com/v2/transfers'
@@ -124,19 +205,12 @@ class Py3WeTransfer(object):
         if self.debug : print(r.text)
         return (json.loads(r.text))['url']
     
-    
-    def file_upload(self, url, file_name, mime_type, bytes_stream):
-        headers = {'Content-Type': mime_type, 'File': file_name}
-        r = requests.put(url, data=bytes_stream)
-    
-    
     def complete_file_upload(self, transfer_id, file_id, part_numbers):
         address = 'https://dev.wetransfer.com/v2/transfers/%s/files/%s/upload-complete' % (transfer_id, file_id)
         headers = { "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
         data    = { "part_numbers": part_numbers }
         if self.debug : print(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
         r = requests.put(address, headers=headers, data=json.dumps(data))
-    
     
     def finalize_transfer(self, transfer_id):
         address = 'https://dev.wetransfer.com/v2/transfers/%s/finalize' % transfer_id
@@ -145,10 +219,9 @@ class Py3WeTransfer(object):
         return (json.loads(r.text))['url']
     
     
-    
     ##################################################################################################################################
     # Warning, 
-    # Bellow this, we use wetransfer API V4,
+    # Bellow this, we use Transfer API V4,
     # Which is currently undocumented, against WeTransfer's CLUF and probably subject to changes...
     ##################################################################################################################################
     def emails(self, sender, recipients, language="en") :
@@ -178,7 +251,6 @@ class Py3WeTransfer(object):
                          'part_numbers':( files[i]['file_size'] // (json.loads(r.text))['files'][0]['chunk_size'] ) + 1,
                          'chunk_size':(json.loads(r.text))['files'][0]['chunk_size'] }
         return [ (json.loads(r.text))['id'], files ]
-    
      
     def request_transfer_mail(self, transfer_id, file_name, file_size):
         address = 'https://wetransfer.com/api/v4/transfers/%s/files' % transfer_id
@@ -189,7 +261,6 @@ class Py3WeTransfer(object):
         if self.debug : print(r.text)
         return [ (json.loads(r.text))['id'], (json.loads(r.text))['chunk_size'] ]
     
-    
     def request_upload_url_mail(self, transfer_id, file_id, part_number, chunk_size, chunk_crc):
         address = 'https://wetransfer.com/api/v4/transfers/%s/files/%s/part-put-url' % (transfer_id, file_id)
         headers = { "Content-Type":"application/json", "x-api-key": self.x_api_key, "Authorization": self.token }
@@ -198,7 +269,6 @@ class Py3WeTransfer(object):
         r = requests.post(address, headers=headers, data=json.dumps(data))
         if self.debug : print(r.text)
         return (json.loads(r.text))['url']
-    
     
     def complete_file_upload_mail(self, transfer_id, file_id, part_numbers):
         address = 'https://wetransfer.com/api/v4/transfers/%s/files/%s/finalize-mpp' % (transfer_id, file_id)
@@ -217,5 +287,3 @@ class Py3WeTransfer(object):
         r = requests.put(address, headers=headers, data=json.dumps(data))
         if self.debug : print(r.text)
         return (json.loads(r.text))['shortened_url']
-    
-    
